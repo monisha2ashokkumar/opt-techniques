c1c83bf43dc223fab4c7137d4725e2a58ae1e2c8
Monisha <monisha2ashokkumar@gmail.com>
2022-03-29 20:04:27 +0800

    optimise-technique

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showPara, setShowPara] = useState(false);
+  const onClickHandler = () => {
+    setShowPara(prevShowPara => !showPara);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h3>Hi hello!!</h3>
+        {showPara && <p>It's me Monisha.. Hello!!!</p>}
+        <button onClick={onClickHandler}>Click me</button>
       </header>
     </div>
   );
